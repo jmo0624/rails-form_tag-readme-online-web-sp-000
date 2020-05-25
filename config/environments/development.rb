d7b0935352770a0ed0f43a53aaf7594741d157b9
Josh Morris <jmo0624@gmail.com>
2020-05-25 15:36:13 +0000

    Automatically backed up by Learn

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   class Application < Rails::Application
-  config.web_console.whitelisted_ips = '<IP address here>'
-end
+    config.web_console.whitelisted_ips = '<IP address here>'
+  end
 end
